a57179416e8dbf9370e92cb7fa997ef2910144be
Simon Gonçalves Costa <simongoncalvescosta@gmail.com>
2024-03-29 18:29:58 -0300

    V2.3

--- script.js
+++ script.js
@@ -85,7 +85,6 @@ function loadItens() {
   itens.forEach((item, index) => {
     insertItem(item, index)
   })
-
 }
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
